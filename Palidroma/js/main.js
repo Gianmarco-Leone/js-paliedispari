50b4ba947cb1503801c9c6d1b688bef3879f9b32
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-01-31 17:32:34 +0100

    modify DOM es. 1

--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -3,21 +3,38 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // SVOLGIMENTO:
+const generateButton = document.getElementById("generate_button");
+
 
 // Chiedi parola
-const userWord = prompt("Inserisci una parola");
+// const userWord = prompt("Inserisci una parola");
 
-// Invoca funzione
-let reverseUserWord = reverseWords(userWord);
 
-// // Stampa parola inversa
-console.log(reverseUserWord);
+generateButton.addEventListener(
+    "click",
+    function () {
+        const textEl = document.getElementById("output_message");
+        let outputMessage;
+        const inputTextEl = document.getElementById("input_word");
+        const userWord = inputTextEl.value;
+        // Invoca funzione
+        let reverseUserWord = reverseWords(userWord);
 
-if (reverseUserWord == userWord) {
-    console.log("La parola è palindroma");
-} else {
-    console.log("La parola non è pallindroma");
-}
+        // // Stampa parola inversa
+        console.log(reverseUserWord);
+
+        if (reverseUserWord == userWord) {
+            console.log("La parola è palindroma");
+            outputMessage = ("La parola " + userWord + " è palindroma");
+        } else {
+            console.log("La parola non è pallindroma");
+            outputMessage = ("La parola " + userWord + " non è pallindroma");
+        }
+        textEl.innerHTML = outputMessage;
+    }
+);
+
+
 
 
 
