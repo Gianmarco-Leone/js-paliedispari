050b9f7533e2e4559b2afb542b838a44ffe7d740
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-01-31 14:14:19 +0100

    end es. 1

--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -3,25 +3,34 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // SVOLGIMENTO:
+
 // Chiedi parola
 const userWord = prompt("Inserisci una parola");
-// Crea variabile parola inversa inizialmente vuota
-let reverseUserWord = "";
-// Selezione carattere per carattere
-let i = userWord.length - 1;
-while (i >= 0) {
-    let letter = userWord[i];
-    // Inserisci nella variabile parola inversa
-    reverseUserWord += letter;
-    console.log(letter);
 
-    i--;
-}
-// Stampa parola inversa
+// Invoca funzione
+let reverseUserWord = reverseWords(userWord);
+
+// // Stampa parola inversa
 console.log(reverseUserWord);
 
 if (reverseUserWord == userWord) {
     console.log("La parola è palindroma");
 } else {
     console.log("La parola non è pallindroma");
+}
+
+
+
+
+
+// Creo function che inverte le parole
+function reverseWords(string) {
+    let reverseWord = "";
+    let i = string.length - 1;
+    while (i >= 0) {
+        let letter = string[i];
+        reverseWord += letter;
+        i--;
+    }
+    return reverseWord;
 }
